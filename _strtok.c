0a484efd7b7b61a0067d8bd0427afc39e29d4b40
abdomarrok <marrokmar@gmail.com>
2023-11-01 14:11:33 -0700

    fix betty strtok

--- _strtok.c
+++ _strtok.c
@@ -17,11 +17,12 @@ char **_strtok(char *string, char *delim)
 	if (!delim)
 		delim = " ";
 	for (i = 0; string[i] != '\0'; i++)
- if (!Is_Delim(string[i], delim) &&(Is_Delim(string[i+1],delim)||!string[i+1]))
-        WordNums++;
-	if (WordNums == 0)
-		return (NULL);
-	s = malloc((1 + WordNums) * sizeof(char *));
+if (!Is_Delim(string[i], delim))
+if((Is_Delim(string[i + 1], delim) || !string[i + 1]))
+WordNums++;
+if (WordNums == 0)
+return (NULL);
+s = malloc((1 + WordNums) * sizeof(char *));
 	if (!s)
 		return (NULL);
 	for (i = 0, j = 0; j < WordNums; j++)
