ec0e8eab40e503ccb5a7ed28659e97df2d4c769c
abdomarrok <marrokmar@gmail.com>
2023-11-01 14:54:37 -0700

    betty Change_str

--- Change_str.c
+++ Change_str.c
@@ -7,36 +7,34 @@
  */
 int Change_vars(Passed_Info_t *Pinfo)
 {
-    int i = 0;
-    MyListString *node;
-
-    for (i = 0; Pinfo->argv[i]; i++)
-    {
-        if (Pinfo->argv[i][0] != '$' || !Pinfo->argv[i][1])
-            continue;
-
-        if (!_StrCompare(Pinfo->argv[i], "$?"))
-        {
-            Change_string(&(Pinfo->argv[i]),
-                          _StrDublicate(ConvertNum(Pinfo->status, 10, 0)));
-            continue;
-        }
-        if (!_StrCompare(Pinfo->argv[i], "$$"))
-        {
-            Change_string(&(Pinfo->argv[i]),
-                          _StrDublicate(ConvertNum(getpid(), 10, 0)));
-            continue;
-        }
-        node = getNodeStartWith(Pinfo->env, &Pinfo->argv[i][1], '=');
-        if (node)
-        {
-            Change_string(&(Pinfo->argv[i]),
-                          _StrDublicate(_LocateChar(node->string, '=') + 1));
-            continue;
-        }
-        Change_string(&Pinfo->argv[i], _StrDublicate(""));
-    }
-    return (0);
+int i = 0;
+MyListString *node;
+for (i = 0; Pinfo->argv[i]; i++)
+{
+if (Pinfo->argv[i][0] != '$' || !Pinfo->argv[i][1])
+continue;
+if (!_StrCompare(Pinfo->argv[i], "$?"))
+{
+Change_string(&(Pinfo->argv[i]),
+_StrDublicate(ConvertNum(Pinfo->status, 10, 0)));
+continue;
+}
+if (!_StrCompare(Pinfo->argv[i], "$$"))
+{
+Change_string(&(Pinfo->argv[i]),
+_StrDublicate(ConvertNum(getpid(), 10, 0)));
+continue;
+}
+node = getNodeStartWith(Pinfo->env, &Pinfo->argv[i][1], '=');
+if (node)
+{
+Change_string(&(Pinfo->argv[i]),
+_StrDublicate(_LocateChar(node->string, '=') + 1));
+continue;
+}
+Change_string(&Pinfo->argv[i], _StrDublicate(""));
+}
+return (0);
 }
 
 /**
@@ -48,7 +46,7 @@ int Change_vars(Passed_Info_t *Pinfo)
  */
 int Change_string(char **old, char *new)
 {
-    free(*old);
-    *old = new;
-    return (1);
+free(*old);
+*old = new;
+return (1);
 }
